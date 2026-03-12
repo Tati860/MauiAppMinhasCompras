4d09c24ffbe8003760cc8efa7443acb248956f63
tati860 <tatipeixoto923@gmail.com>
2026-03-12 15:51:07 -0300

    Atualizar alteraçoes

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,9 +1,59 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> Lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = Lista;
 	}
+    protected  async override void OnAppearing()
+    {
+		List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach(i => Lista.Add(i));
+    }
+
+	private void ToolbarItem_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Navigation.PushAsync(new Views.NovoProduto());
+		}
+        catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+	}
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		Lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => Lista.Add(i));
+
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = Lista.Sum(i => i.Total);
+		string msg = $"O total é{soma:C2}";
+		DisplayAlert("Total dos produtos", msg, "OK");
+
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,36 @@
+using MauiAppMinhasCompras.Models;
+using MauiAppMinhasCompras.Views;
+using System.Threading.Tasks;
+
+
 namespace MauiAppMinhasCompras.Views;
 
-public partial class NovoItem : ContentPage
+public partial class NovoProduto : ContentPage
 {
-	public NovoItem()
+	public NovoProduto()
 	{
 		InitializeComponent();
 	}
+    
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso", "Registro Inserido", "OK");
+			
+
+		}catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+
+    }
 }
